d753a565e0913571f3607f995ca70390aed0baa1
getong003 <getong003@ke.com>
2023-09-27 18:18:22 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import invitation from "./invitation.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={invitation} />
+    </div>
+  );
+}
+
+export default App;
